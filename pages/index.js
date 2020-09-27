41d7eac91458f3dd7e3301b63a4d4637c0ae6346
Viktor Hollanders <viktorhollanders@gmail.com>
2020-09-27 19:57:57 +0000

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,159 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Stroka</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="preload"
+          href="/fonts/waldorfskrift.ttf"
+          as="font"
+          crossOrigin=""
+        />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main>
+        <header>
+          <img className="logo" src="/images/logo.png" />
+          <h1 className="logo-text">STROKA</h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <div className="openingHouers">
+            <p>
+              Mánudag <strong>11–14</strong> og <strong>16–17</strong>
+            </p>
+            <p>
+              Miðvikudag <strong>11–14</strong>
+            </p>
+          </div>
+        </header>
+
+        <p className="text">
+          Við erum að setja nýa vefsíðu í loftið vef verslunin verður óvirk á
+          meðal en hér að neðan er hægt að finna helstu upplýsingar um Stroku.
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <footer>
+          <div className="footer__contact-wrapper">
+            <div className="contact-item">
+              <h3 className="contact-title">Staðsetning</h3>
+              <p className="contact-text">Nýbýlavegi 8 (Portið),</p>
+              <p className="contact-text">200 Kópavogur</p>
+            </div>
+            <div className="contact-item">
+              <h3 className="contact-title">Hafa samband</h3>
+              <p className="contact-text">S: 699-3096</p>
+              <a src="mailto:stroka.mail@gmail.com">stroka.mail@gmail.com</a>
+            </div>
+            <div className="contact-item">
+              <h3 className="contact-title">Fylgstu með</h3>
+              <div className="contact-images">
+                <img
+                  className="contact-image"
+                  src="/images/stroka facebook.png"
+                />
+                <img
+                  className="contact-image"
+                  src="/images/stroka instagram.png"
+                />
+              </div>
+            </div>
+          </div>
+          <p className="footer__tax-info">
+            Sigríður Þorbergsdóttir | Kt: 030572-5649 |banki: 0130-26-013087 |
+            Vsk númer: 131706
+          </p>
+        </footer>
+        <style jsx>{`
+          main {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+          }
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          /* header */
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+          header {
+            width: 100vw;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+            padding: 112px 0 48px;
+          }
+
+          .logo {
+            height: 160px;
+            width: 160px;
+          }
+
+          .logo-text {
+            font-family: "Waldorf-skrift";
+            text-align: center;
+            color: #5b2e03;
+          }
+
+          .openingHouers {
+            margin-top: px;
+          }
+
+          .openingHouers p {
+            text-align: center;
+          }
+
+          .text {
+            max-width: 500px;
+            margin: 100px 16px;
+          }
+
+          /* footer */
+
+          footer {
+            color: #5b2e03;
+          }
+
+          .footer__contact-wrapper {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+          }
+
+          .contact-item {
+            margin-bottom: 30px;
+            max-width: 200px;
+            text-align: center;
+          }
+
+          .contact-title {
+            color: #5b2e03;
+          }
+
+          .contact-images {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-around;
+          }
+
+          .contact-image {
+            width: 32px;
+            height: 32px;
+          }
+
+          .footer__tax-info {
+            margin: 18px 16px;
+          }
+
+          @media screen and (min-width: 760px) {
+            .footer__contact-wrapper {
+              flex-direction: row;
+              flex-wrap: wrap;
+              align-items: flex-start;
+              justify-content: space-around;
+            }
+          }
+        `}</style>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
