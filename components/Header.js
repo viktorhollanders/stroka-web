8f17f31fd6edf739bf44bf34044f22093d47faa8
Viktor Hollanders <viktorhollanders@gmail.com>
2020-11-26 20:45:25 +0000

    add header

--- components/Header.js
+++ components/Header.js
@@ -9,26 +9,53 @@ export default function Nav() {
           <h1 className="header__logo-text">STROKA</h1>
         </Link>
 
-        <nav className="header__nav">
+        <nav className="headerNav">
           <Link href="/products">
-            <a>Vörur</a>
+            <a className="headerNav__item">Vörur</a>
           </Link>
           <Link href="/">
-            <a>Meðferðir</a>
+            <a className="headerNav__item">Meðferðir</a>
           </Link>
           <Link href="/">
-            <a>Um Stroku</a>
+            <a className="headerNav__item">Um Stroku</a>
           </Link>
         </nav>
       </header>
 
       <style jsx>{`
+        header {
+          position: fixed;
+          width: 100vw;
+          background-color: #fff;
+          z-index: 1;
+          top: 0;
+          left: 0;
+
+          padding: 48px 24px 0px 24px;
+        }
+
+        .header__logo-text {
+          font-size: 24px;
+          text-align: center;
+          margin: 0;
+        }
+
         .header__logo-text {
           font-family: "Waldorf-skrift";
           color: #5b2e03;
         }
 
-        .header__nav {
+        .headerNav {
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+
+          padding: 16px 0 9px 0;
+        }
+
+        .headerNav__item {
+          font-size: 14px;
+          font-weight: 600;
           color: #5b2e03;
         }
       `}</style>
