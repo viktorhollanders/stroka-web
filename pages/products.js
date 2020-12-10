9546b8757d3d951531743fc5d02b79550b9c7cc6
Viktor Hollanders <viktorhollanders@gmail.com>
2020-12-10 21:07:49 +0000

    finish styling up product for tablet and desktop

--- pages/products.js
+++ pages/products.js
@@ -1,10 +1,11 @@
+import Prismic from "prismic-javascript";
 import Head from "next/head";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Prismic from "prismic-javascript";
 import { RichText } from "prismic-reactjs";
+import Link from "next/link";
 
-export default function Products({ products }) {
+function Products({ products, catagotys }) {
   return (
     <div>
       <Head>
@@ -28,13 +29,13 @@ export default function Products({ products }) {
 
         <section className="products">
           {products.map((product) => (
-            <article classname="product" key={product.id}>
+            <article className="product" key={product.id}>
               <img
                 className="product__image"
                 src={product.data.images[0].image.url}
                 alt={product.data.title}
               />
-              <h1 className="product__price">5.385 .kr</h1>
+              <h1 className="product__price">{product.data.price} .kr</h1>
               <p className="product__title">
                 {RichText.asText(product.data.title)}
               </p>
@@ -80,6 +81,23 @@ export default function Products({ products }) {
 
         /* producs */
 
+        .products {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+        }
+
+        .product {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+
+          background-color: #fff;
+          box-shadow: 0px 4px 40px rgba(91, 46, 3, 0.11);
+          border-radius: 15px;
+          padding: 16px;
+          margin: 13px;
+        }
+
         .product__image {
           height: 101px;
           width: 101px;
@@ -89,11 +107,39 @@ export default function Products({ products }) {
         .product__price {
           font-size: 24px;
           font-weight: 600;
+          width: 133px;
+          margin: 23px 0 16px 0;
         }
 
         .product__title {
           font-size: 14px;
+          font-weight: 600;
           width: 133px;
+          margin: 0;
+        }
+
+        @media screen and (min-width: 760px) {
+          .products {
+            grid-template-columns: 1fr 1fr 1fr;
+          }
+          .product {
+            margin: 20px 40px;
+          }
+        }
+
+        @media screen and (min-width: 1000px) {
+          .products {
+            grid-template-columns: 1fr 1fr 1fr 1fr;
+          }
+          .product {
+            margin: 20px 40px;
+          }
+        }
+
+        @media screen and (min-width: 1200px) {
+          .product {
+            margin: 40px 80px;
+          }
         }
       `}</style>
     </div>
@@ -103,9 +149,22 @@ export default function Products({ products }) {
 export async function getServerSideProps() {
   const apiEndpoint = "https://stroka.cdn.prismic.io/api/v2";
   const client = Prismic.client(apiEndpoint);
+
   const productsResponse = await client.query(
-    Prismic.Predicates.at("document.type", "product")
+    Prismic.Predicates.at("document.type", "product"),
+    { pageSize: 300 }
   );
 
-  return { props: { products: productsResponse.results } };
+  const catagoryResponse = await client.query(
+    Prismic.Predicates.at("document.type", "catagory")
+  );
+
+  return {
+    props: {
+      products: productsResponse.results,
+      catagotys: catagoryResponse.results,
+    },
+  };
 }
+
+export default Products;
